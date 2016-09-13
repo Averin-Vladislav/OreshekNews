151fbe5586249be56a83b05e31918ad8bb84b868
Uladzislau Averyn <Uladzislau_Averyn@epam.com>
2016-09-13 15:47:36 +0300

    edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
-﻿const gulp   = require('gulp'),
-      babel  = require('gulp-babel'),
-      sass   = require('gulp-sass'),
-      concat = require('gulp-concat'),
-      uglify = require('gulp-uglifyjs');
+﻿const gulp         = require('gulp'),
+      babel        = require('gulp-babel'),
+      sass         = require('gulp-sass'),
+      concat       = require('gulp-concat'),
+      uglifyjs     = require('gulp-uglifyjs'),
+      autoprefixer = require('gulp-autoprefixer'),
+      uglifycss    = require('gulp-uglifycss');
+
  
 gulp.task('babel', () => {
     return gulp.src('js/app.js')
@@ -16,18 +19,33 @@ gulp.task('scripts', () => {
     return gulp.src(['libraries/angular.min.js',
                      'libraries/jquery-3.1.0.min.js'])
             .pipe(concat('libs.min.js'))
-            .pipe(uglify())
+            .pipe(uglifyjs())
             .pipe(gulp.dest('libraries/build'));
+});
+
+gulp.task('styles', () => {
+    return gulp.src(['libraries/reset/css',
+                     'libraries/bootstrap-theme.css',
+                     'libraries/bootstrap.css',])
+        .pipe(concat('libs.min.css'))
+        .pipe(uglifycss())
+        .pipe(gulp.dest('libraries/build'));
 });
 
 gulp.task('sass', () => {
     return gulp.src('sass/style.scss')
         .pipe(sass())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.src('css/style.css'))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(uglifycss())
+        .pipe(gulp.dest('css/build'));
 });
 
 gulp.task('watch', () => {
     gulp.watch('sass/style.scss', ['sass']);
     gulp.watch('js/app.js', ['babel']);
     gulp.watch('libraries/*.js', ['scripts']);
+    gulp.watch('libraries/*css', ['styles']);
 });
